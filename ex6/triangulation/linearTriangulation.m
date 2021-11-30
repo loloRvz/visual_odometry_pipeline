a38e8edd52b2522a6e43db04f8548dc13b2839cd
loloRvz <rouvinez.laurent@gmail.com>
2021-11-30 15:38:25 +0100

    began init

---
+++ ex6/triangulation/linearTriangulation.m
@@ -0,0 +1,40 @@
+function P = linearTriangulation(p1,p2,M1,M2)
+% LINEARTRIANGULATION  Linear Triangulation
+%
+% Input:
+%  - p1(3,N): homogeneous coordinates of points in image 1
+%  - p2(3,N): homogeneous coordinates of points in image 2
+%  - M1(3,4): projection matrix corresponding to first image
+%  - M2(3,4): projection matrix corresponding to second image
+%
+% Output:
+%  - P(4,N): homogeneous coordinates of 3-D points
+
+% Sanity checks
+[dim,NumPoints] = size(p1);
+[dim2,npoints2] = size(p2);
+assert(dim==dim2,'Size mismatch of input points');
+assert(NumPoints==npoints2,'Size mismatch of input points');
+assert(dim==3,'Arguments x1, x2 should be 3xN matrices (homogeneous coords)');
+
+[rows,cols] = size(M1);
+assert(rows==3 && cols==4,'Projection matrices should be of size 3x4');
+[rows,cols] = size(M2);
+assert(rows==3 && cols==4,'Projection matrices should be of size 3x4');
+
+P = zeros(4,NumPoints);
+
+for i=1:NumPoints
+    % Built matrix of linear homogeneous system of equations
+    A = [cross2Matrix(p1(:,i))*M1;
+         cross2Matrix(p2(:,i))*M2];
+
+    % Solve the linear homogeneous system of equations
+    [~,~,v] = svd(A,0);
+    P(:,i) = v(:,4);
+end
+
+% Dehomogeneize (P is expressed in homogeneous coordinates)
+P = P./repmat(P(4,:),4,1);
+
+end
--- main.m
+++ main.m
@@ -5,9 +5,10 @@ close all;
 %% Setup
 ds = 2; % 0: KITTI, 1: Malaga, 2: parking
 
-kitti_path = './kitti';
-malaga_path = './malaga-urban-dataset-extract-07';
-parking_path = './parking';
+kitti_path = './data/kitti';
+malaga_path = './data/malaga-urban-dataset-extract-07';
+parking_path = './data/parking';
+addpath(genpath(pwd));
 
 if ds == 0
     % need to set kitti_path to folder containing "05" and "poses"
@@ -42,7 +43,7 @@ end
 
 %% Bootstrap
 bootstrap_frames(1) = 0;
-bootstrap_frames(2) = 2;
+bootstrap_frames(2) = 5;
 
 if ds == 0
     img0 = imread([kitti_path '/05/image_0/' ...
@@ -64,6 +65,98 @@ elseif ds == 2
 else
     assert(false);
 end
+
+%Keypoint correspondences between first 2 frames (Harris)
+corner_patch_size = 9;
+harris_kappa = 0.08;
+num_keypoints = 200;
+nonmaximum_supression_radius = 8;
+descriptor_radius = 9;
+match_lambda = 4;
+
+%Harris Scores
+harris_scores0 = harris(img0, corner_patch_size, harris_kappa);
+harris_scores1 = harris(img1, corner_patch_size, harris_kappa);
+%Keypoints
+keypoints0 = selectKeypoints(...
+    harris_scores0, num_keypoints, nonmaximum_supression_radius);
+keypoints1 = selectKeypoints(...
+    harris_scores1, num_keypoints, nonmaximum_supression_radius);
+% figure(1);
+% imshow(img0);
+% hold on;
+% plot(keypoints0(2, :), keypoints0(1, :), 'rx', 'Linewidth', 2);
+%Keypoint descriptors
+descriptors0 = describeKeypoints(img0, keypoints0, descriptor_radius);
+descriptors1 = describeKeypoints(img1, keypoints1, descriptor_radius);
+
+%Match descriptors
+matches = matchDescriptors(descriptors1, descriptors0, match_lambda);
+[~, indx1, indx0] = find(matches);
+matched_points0 = keypoints0(:,indx0);
+matched_points1 = keypoints1(:,indx1);
+
+figure(2);
+imshow(img1);
+hold on;
+plot(keypoints1(2, :), keypoints1(1, :), 'rx', 'Linewidth', 2);
+plot([matched_points0(2,:); matched_points1(2,:)], ...
+     [matched_points0(1,:); matched_points1(1,:)], ...
+     'g-', 'Linewidth', 3);
+ 
+%Flip from matrix index to image coordinates
+matched_points0 = flip(matched_points0);
+matched_points1 = flip(matched_points1);
+
+%Fundamental matrix
+[F,inliersIndex] = estimateFundamentalMatrix(matched_points0',...
+                                             matched_points1',...
+                                             'Method','RANSAC'); 
+%Essential matrix
+E = K'*F*K;
+
+%Only keep inliers
+matched_points0 = matched_points0(:,inliersIndex);
+matched_points1 = matched_points1(:,inliersIndex);
+
+%Rotation, Translation
+matched_points0 = [matched_points0;ones(1,length(matched_points0))];
+matched_points1 = [matched_points1;ones(1,length(matched_points1))];
+[R,u3] = decomposeEssentialMatrix(E);
+[R_C_W,T_C_W] = disambiguateRelativePose(R,u3,matched_points0,matched_points1,K,K);
+
+% Triangulate
+M1 = K * eye(3,4);
+M2 = K * [R_C_W, T_C_W];
+P = linearTriangulation(matched_points0,matched_points1,M1,M2);
+
+figure(3),
+subplot(1,3,1)
+plot3(P(1,:), P(2,:), P(3,:), 'o');
+plotCoordinateFrame(eye(3),zeros(3,1), 0.8);
+text(-0.1,-0.1,-0.1,'Cam 1','fontsize',10,'color','k','FontWeight','bold');
+
+center_cam2_W = -R_C_W'*T_C_W;
+plotCoordinateFrame(R_C_W',center_cam2_W, 0.8);
+text(center_cam2_W(1)-0.1, center_cam2_W(2)-0.1, center_cam2_W(3)-0.1,'Cam 2','fontsize',10,'color','k','FontWeight','bold');
+
+axis equal
+rotate3d on;
+grid
+
+% Display matched points
+subplot(1,3,2)
+imshow(img0);
+hold on
+plot(matched_points0(1,:), matched_points0(2,:), 'ys');
+title('Image 0')
+
+subplot(1,3,3)
+imshow(img1);
+hold on
+plot(matched_points1(1,:), matched_points1(2,:), 'ys');
+title('Image 1')
+
 
 %% Continuous operation
 range = (bootstrap_frames(2)+1):last_frame;

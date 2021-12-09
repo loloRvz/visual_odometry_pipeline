dd1a9b342b8f0309fdcfc6f71042e71525b65de8
samibouziri <bouziri.sami@gmail.com>
2021-12-09 16:12:16 +0100

    cvo improvement

--- CVO_test/triangulate2.m
+++ CVO_test/triangulate2.m
@@ -1,10 +1,12 @@
-function [newP,newX,C_i,f_i,T_i] = triangulate2dto3d(C_i,f_i,T_i,R_C_W, t_W_C,K)
-    newP = zeros(size(C_i));
-    newX = zeros(size(C_i,1),3);
+function [newP,newX,C_i,f_i,T_i] = triangulate2(C_i,f_i,T_i,R_C_W, t_W_C,K,C_i_1,R_C_W_1, t_W_C_1)
+    newP = [];
+    newX = [];
     keep = true(size(C_i,1),1);
     TCW = -R_C_W*t_W_C';
+    TCW_1 = -R_C_W_1*t_W_C_1';
     M2 = K*[R_C_W TCW];
-    parfor i = 1:size(C_i,1)
+    M3 = K*[R_C_W_1 TCW_1];
+    for i = 1:size(C_i,1)
        % if (norm(C_i(i,:)-f_i(i,:)))>25
         RWC_i = reshape(T_i(i,1:9),3,3);
         TWC_i = T_i(i,10:12)';
@@ -18,18 +20,21 @@ function [newP,newX,C_i,f_i,T_i] = triangulate2dto3d(C_i,f_i,T_i,R_C_W, t_W_C,K)
         P = P (1:3)/P (end);
         P1 = R_C_W*(P-t_W_C');
         P2 = RWC_i'*(P-TWC_i);
-        if (P1(3)<=0.5|| P2(3)<=0.5)
+        if (P1(3)<=0.7 || P2(3)<=0.7)
             continue
         end
 %         csalpha = (P-t_W_C')'*(P-TWC_i)/norm(P-t_W_C')/norm(P-TWC_i);
-        if abs(atan2(norm(cross(P-t_W_C',P-TWC_i)), dot(P-t_W_C',P-TWC_i)))>1.5*pi/180
+        p3x = cross2Matrix([C_i_1(i,:)';1]);
+        A = [p1x*M1;p3x*M3];
+        [~,~,V] = svd(A);
+        P3 = V(:,end);
+        P3 = P3 (1:3)/P3 (end);
+        if norm(P-P3)<0.05
         keep(i) = false;
-        newP(i,:) = C_i(i,:);
-        newX(i,:) = P';
+        newP(end+1,:) = C_i(i,:);
+        newX(end+1,:) = P';
         end
     end
-    newP = newP(not(keep),:);
-    newX = newX(not(keep),:);
     C_i = C_i(keep,:);
     f_i = f_i(keep,:);
     T_i = T_i(keep,:);
